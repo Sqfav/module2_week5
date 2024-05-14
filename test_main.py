bf146ca60e68f784563b6d25022022aea0a79a6f
Sqfav <igor.mozzy@outlook.com>
2024-05-14 17:04:25 +0200

    исправлен тест

--- test_main.py
+++ test_main.py
@@ -1,22 +1,24 @@
 from fastapi.testclient import TestClient
-
+import time
 from main import app
 
-client = TestClient(app)
 
-
+# использование TestClient для подобных событий
+# https://stackoverflow.com/questions/75714883/how-to-test-a-fastapi-endpoint-that-uses-lifespan-function
+# https://fastapi.tiangolo.com/advanced/testing-events/
+# https://fastapi.tiangolo.com/advanced/events/
 def test_create_task():
-    response = client.post("/task", json={"duration": 1})
-    assert response.status_code == 200
-    task_id = response.json()["task_id"]
+    with TestClient(app) as client:
+        response = client.post("/task", json={"duration": 1})
+        assert response.status_code == 200
+        task_id = response.json()["task_id"]
 
-    response = client.get(f"/task/{task_id}")
-    assert response.status_code == 200
-    assert response.json() == {"status": "running"}
+        response = client.get(f"/task/{task_id}")
+        assert response.status_code == 200
+        assert response.json() == {"status": "running"}
 
-    # этот тест так и не получилось заставить работать :(((
-    # time.sleep(2)
-    #
-    # response = client.get(f"/task/{task_id}")
-    # assert response.status_code == 200
-    # assert response.json() == {"status": "done"}
+        time.sleep(2)
+
+        response = client.get(f"/task/{task_id}")
+        assert response.status_code == 200
+        assert response.json() == {"status": "done"}
